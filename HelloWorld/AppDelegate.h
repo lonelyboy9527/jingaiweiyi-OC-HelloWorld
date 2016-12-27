61b2156acbd7e0647c05b047eef37aa697698ba2
tang fei <lonelyboy9527@sina.com>
2016-12-27 15:35:26 +0800

    我的修改

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by jingaiweiyi on 2016/12/27.
+//  Copyright © 2016年 yunshangshiji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
